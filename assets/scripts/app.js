81540fa33c4ae1386f59bc13b3469a2b877b5c76
RichardAybar <theenvykillz@gmail.com>
2018-11-30 09:28:47 -0500

    Merging to master

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,20 +6,18 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-
-
-  const authEvents = require('./auth/events.js')
-  //document ready, doesnt run code until html is done loading
+const authEvents = require('./auth/events.js')
+// document ready, doesnt run code until html is done loading
 $(() => {
-  //selects html ID and .on attaches event listener to ID,
-  //.on takes 2 paramters, event tht it is listening for and the callback function that uses that event
+  // selects html ID and .on attaches event listener to ID,
+  // .on takes 2 paramters, event tht it is listening for and the callback function that uses that event
   $('#sign-up-form').on('submit', authEvents.onSignUp)
   $('#sign-in-form').on('submit', authEvents.onSignIn)
   $('#change-password-form').on('submit', authEvents.onChangePassword)
   $('#sign-out-button').on('submit', authEvents.onSignOut)
 })
 
-  // const events = require('./auth/events.js')
+// const events = require('./auth/events.js')
 // HTML -  getFormFields - ID
 //   html submit is clickeD events.onsIGNUP
 //   $(ID).onSubmit(events.onsignup)
@@ -30,4 +28,4 @@ $(() => {
 //   password_confirmation: 'value123'
 // }})
 
-  // your JS code goes here
+// your JS code goes here
--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,20 +1,67 @@
 const api = require('./api.js')
+const ui = require('./ui.js')
 const getFormFields = require('../../../lib/get-form-fields.js')
 // const ui = require('./ui.js')
 
-//onSignUp receives event object from app.js/.on
+// onSignUp receives event object from app.js/.on
 
-const onSignUp = (event) => {
+
+// onSignUp receives event object from app.js/.on
+const onSignUp = event => {
   event.preventDefault()
-  const data = getFormFields(event.target) //  return {name: asdasd passswoird: dsafsdf}
+  const data = getFormFields(event.target)
+  $(event.target).trigger('reset')
   // data is sent to SignUp fucntion is api.js
   api.signUp(data)
-.then((responsefromAPI) => {//.then is  function that takes a call back > response (object from api) and does something with it,
-      console.log(responsefromAPI.email)
-    })
-}
+    .then(ui.signUpSuccess)  // .then is  function that takes a call back > response (object from api) and does something with it,
+    .catch(ui.signUpFailure)
+
+const onSignIn = event => {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  $(event.target).trigger('reset')
+  api.signIn(data)
+    .then(ui.signInSuccess) // if your request was succesful
+    .catch(ui.signInFailure) // if your request failed
+
+  const onSignOut = event => {
+    event.preventDefault()
+    api.signOut()
+      .then(ui.signOutSuccess)
+      .catch(ui.signOutFailure)
+  }
+
+  const onChangePassword = event => {
+    event.preventDefault()
+      const data = getFormFields(event.target)
+      $(event.target).trigger('reset')
+      api.changePassword(data)
+        .then(ui.changePasswordSuccess)
+        .catch(ui.failure)
+    }
 
 
 module.exports = {
-  onSignUp
+  onSignUp,
+  onSignIn,
+  onSignOut,
+  onChangePassword,
+  getFormFields
 }
+
+
+
+// UI FILE
+// SHOWNOTES (NOTES) => {
+//NOTES.FOREACH((NOTE)=>{
+//  $(NOTES-CONTAINER).APPEND(NOTE.MESSAGE)
+//})
+//}
+
+//EXPORT ALL ITS FUNCITON
+//[{},{},{}]
+
+
+
+
+//ONSIGNIN AND I THEN(RESOIBSE) CALLBACK -> UI.SHOWNOTES(RESPNSE)
