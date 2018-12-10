10cf8a196059e93ea9a7a99f6a01a9c304974880
RichardAybar <theenvykillz@gmail.com>
2018-12-10 09:27:27 -0500

    Merge

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -13,6 +13,17 @@ const signUp = (data) => {
   })
 }
 
+const signIn = (data) => {
+  //g ets data from events.js
+  //ajax is a function that take in an object as a parameter
+  //POST is  a restful action /CRUD
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: {credentials: data}
+  })
+}
+
 module.exports = {
   signUp
 }
